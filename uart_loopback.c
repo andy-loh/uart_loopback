4c938a807657eb6d4d00794e6dfc596bc32d15be
andy.loh <andy.loh@starfivetech.com>
2022-09-27 13:16:48 +0800

    fix error

--- uart_loopback.c
+++ uart_loopback.c
@@ -149,12 +149,11 @@ int main(int argc, char const *argv[])
 			max_index = i;
 			printf("max index is %d\n", max_index);
 		}
-
-		if ( max_index <= min_index || max_index <= 0 || min_index <= 0 ) {
-			printf("Error in baud rate finding!\n");
-			err = FAILURE;
-			goto end;
-		}
+	}
+	if ( max_index <= min_index || max_index <= 0 || min_index <= 0 ) {
+		printf("Error in baud rate finding!\n");
+		err = FAILURE;
+		goto end;
 	}
 	// open the uart device for reading and writing || not control tty because
 	// we don't want to get killed if linenoise sends CTRL-C || open the device
